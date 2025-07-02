96fc20cbdda6abcf8b51c9d0394cdd337fb0107d
Nikita <n.safono@gmail.com>
2025-07-02 17:56:50 +0300

    second commit(repository, service)

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/safonon/todo-app"
 	handler "github.com/safonon/todo-app/pkg/handlers"
+	"github.com/safonon/todo-app/pkg/repository"
+	"github.com/safonon/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
