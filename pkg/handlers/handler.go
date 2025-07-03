28562dfe476a3217af0cf225b3331107b401a47c
Nikita <n.safono@gmail.com>
2025-07-04 00:03:15 +0300

    list task

--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -22,13 +22,13 @@ func (h *Handler) InitRoutes() *gin.Engine {
 		auth.POST("/sign-in", h.signIn)
 	}
 
-	api := router.Group("/api")
+	api := router.Group("/api", h.userIdentity)
 	{
 		lists := api.Group("/lists")
 		{
 			lists.POST("/", h.createList)
 			lists.GET("/", h.getAllLists)
-			lists.GET("/:id", h.getListsById)
+			lists.GET("/:id", h.getListById)
 			lists.PUT("/:id", h.updateList)
 			lists.DELETE("/:id", h.deleteList)
 
